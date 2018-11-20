f750d829274b035e728240b6019e596f67629c39
haotian <hl7gr@virginia.edu>
2018-11-20 01:25:48 -0500

    node

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Node {
+
+    int val = -1;
+    boolean visited = false;
+    Set<Node> neighbors = new HashSet<>();
+
+    public Node(){
+        //default constructor
+
+    }
+
+    public Node(int val){
+        this.val = val;
+    }
+
 }
