4b35abee91cf38deb0b43506ea6a24aba2bb9382
haotian <hl7gr@virginia.edu>
2018-11-23 14:14:38 -0500

    refactored list impl

--- src/graph/unweighted/undirected/Graph.java
+++ src/graph/unweighted/undirected/Graph.java
@@ -1,101 +1,51 @@
 package graph.unweighted.undirected;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Graph {
 
-    /*
-    * Wrapper class of Node: graph representation
-    * */
+    /* adjacency list implementation
+     * generate graph containing nodes [0, this.numNode)
+     * implementation
+      * */
 
-    Set<Node> nodes = new HashSet<>();
+    int numNodes;
+    List<LinkedList<Integer>> list;
 
     public Graph(){
         //default constructor
+        this.numNodes = -1;
     }
 
-    public Graph(int[] nums){
-        for(Integer i : nums){
-            Node newNode = new Node(i);
-            this.nodes.add(newNode);
+    public Graph(int numNodes){
+        this.numNodes = numNodes;
+        this.list = new ArrayList<>();
+        //init
+        for(int i = 0; i < this.numNodes; ++ i){
+            this.list[i] = new LinkedList<>();
         }
     }
 
-    public void addEdge(int i, int j) throws NoSuchFieldException {
-        if(!contains(this.nodes, i) || !contains(this.nodes, j)){
-            throw new NoSuchFieldException("Malformed input!");
+    public void addEdge(int i, int j){
+        //check input
+        if(i < 0 || i >= this.numNodes || j < 0 || j >= this.numNodes){
+            throw new IndexOutOfBoundsException("Malformed input!");
         }
-        get(this.nodes, i).children.add(new Node(j));
-        get(this.nodes, j).children.add(new Node(i));   //undirected graph
-    }
-
-    /*custom contains method*/
-    private static boolean contains(Set<Node> set, int n){
-        for(Node node : set){
-            if(node.val == n){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /*custom get method*/
-    private static Node get(Set<Node> set, int i){
-        Node result = new Node();
-        for(Node n : set){
-            if(n.val == i){
-                result = n;
-            }
-        }
-        return result;
-    }
-
-    /*custom getChildren method*/
-    private Set<Node> getChildren(int i){
-        Set<Node> result = new HashSet();
-        for(Node n : this.nodes){
-            if(n.val == i){
-                for(Node child : n.children){
-                    result.add(child);
-                }
-            }
-        }
-        return result;
+        //add two edges
+        this.list[i].offerLast(j);
+        this.list[j].offerLast(i);
     }
 
     public void visualize(){
-        for(Node n : this.nodes){
-            System.out.println(n.toString());
-        }
-        System.out.println();   //new line separator
-    }
-
-
-    /*find shortest number of edges connecting two nodes, i -> j
-    * TODO: make this work!*/
-    public List<Integer> findMinDist(int i, int j){
-        //classic BFS approach, queue of routes
-        LinkedList<List<Integer>> queue = new LinkedList<>();
-        List<Integer> currPath = new ArrayList<>();
-        currPath.add(i);
-        queue.add(currPath);
-        while(!queue.isEmpty()){
-            List<Integer> path = queue.remove();
-            Node lastNode = get(this.nodes, path.get(path.size() - 1));
-
-            //return condition
-            if(lastNode.val == j){
-                return path;    //assume a path will always exist
+        for(int i = 0; i < this.numNodes; ++ i){
+            System.out.print("node " + i + ":");
+            for(Integer child : this.list[i]){
+                System.out.print(" " + child);
             }
-
-            Set<Node> children = getChildren(lastNode.val);
-            for(Node n : children){
-                currPath.add(n.val);
-                queue.add(new ArrayList<>(currPath));
-                currPath.remove(currPath.size() - 1);
-            }
+            System.out.println();   //new line separator
         }
-        return null;
     }
 
 }
--- src/graph/unweighted/undirected/Main.java
+++ src/graph/unweighted/undirected/Main.java
@@ -1,23 +1,9 @@
 package graph.unweighted.undirected;
 
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) throws NoSuchFieldException {
-        int[] nums = {1, 2, 3, 4, 5, 6};
-        Graph g = new Graph(nums);
-        g.addEdge(4, 5);
-        g.addEdge(1, 5);
-        g.addEdge(1, 4);
-        g.addEdge(1, 6);
-        g.addEdge(3, 4);
-        g.addEdge(1, 3);
-        g.addEdge(2, 3);
-        g.addEdge(1, 2);
-        g.visualize();
-        List<Integer> result = g.findMinDist(2, 5);
-        for(Integer i : result){
-            System.out.print(i);
-        }
+    /* main testing class */
+
+    public static void main(String[] args){
+
     }
 }
