239d72fc6e27e86ae152161b9b9d44418d9c84d3
haotian <hl7gr@virginia.edu>
2018-11-23 15:16:01 -0500

    directed wegithed graph impl

--- src/graph/weighted/directed/Graph.java
+++ src/graph/weighted/directed/Graph.java
@@ -12,7 +12,7 @@ public class Graph {
     * */
 
     int numNodes;
-    List<LinkedList<Integer>> list;
+    List<LinkedList<Edge>> list;
 
     public Graph(){
         //default constructor
@@ -27,6 +27,31 @@ public class Graph {
         }
     }
 
-    
+    public void addEdge(int src, int dest, int weight){
+        //check input
+        if(src < 0 || src >= this.numNodes || dest < 0 || dest >= this.numNodes || weight < 0){
+            throw new IndexOutOfBoundsException("Malformed input!");
+        }
+        //newEdge
+        Edge newEdge = new Edge(src, dest, weight);
+        this.list.get(src).add(newEdge);
+    }
 
+    public int getOutdegree(int node){
+        if(node < 0 || node >= this.numNodes){
+            throw new IndexOutOfBoundsException("Malformed input!");
+        }
+        return this.list.get(node).size();
+    }
+
+    public void visualize(){
+        for(int i = 0; i < this.numNodes; ++ i){
+            System.out.println("node " + i + ": ");
+            LinkedList<Edge> edgeList = this.list.get(i);
+            for(Edge e : edgeList){
+                System.out.println("    " + "(" + e.src + " -> " + e.dest + ") with weight " + e.weight);
+            }
+            System.out.println();   //new line separator
+        }
+    }
 }
--- src/graph/weighted/directed/Main.java
+++ src/graph/weighted/directed/Main.java
@@ -7,6 +7,18 @@ public class Main {
     * */
 
     public static void main(String[] args){
+        Graph g = new Graph(7);
 
+        g.addEdge(2, 1, 1);
+        g.addEdge(2, 0, 3);
+        g.addEdge(6, 2, 7);
+        g.addEdge(0, 6, 5);
+        g.addEdge(3, 2, 8);
+        g.addEdge(3, 6, 29);
+        g.addEdge(6, 5, 569);
+        g.addEdge(3, 4, 11);
+        g.addEdge(4, 5, 47);
+
+        g.visualize();
     }
 }
